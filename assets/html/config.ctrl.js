abc4111590eb6a15b2ee5361fcff9edf752634d4
Viktor Farcic <viktor.farcic@everis.com>
2014-07-07 10:42:17 +0200

    Added running screen

--- assets/html/config.ctrl.js
+++ assets/html/config.ctrl.js
@@ -5,12 +5,16 @@ angular.module('configModule', ['ngTable'])
             $locationProvider.html5Mode(true);
             $routeProvider
                 .when('/page/home', {
-                    templateUrl: '/assets/html/home/home.tmpl.html',
-                    controller: 'homeCtrl'
+                    templateUrl: '/assets/html/home/home.tmpl.html'
                 })
                 .when('/page/books', {
                     templateUrl: '/assets/html/books/books.tmpl.html',
                     controller: 'booksCtrl'
+                })
+                .when('/page/static/:name', {
+                    templateUrl: function(params) {
+                        return '/assets/html/static/' + params.name + '.tmpl.html';
+                    }
                 })
                 .otherwise({
                     redirectTo: '/page/home'
